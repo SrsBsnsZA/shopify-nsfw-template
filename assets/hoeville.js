c24b735c9c169f3b0cad1d6fc3242f2ef5c0a59a
TallmanCode <steve@tallmancode.co.za>
2024-06-19 23:41:37 +0200

    hoevile

--- assets/hoeville.js
+++ assets/hoeville.js
@@ -1,70 +1,90 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const wrapper = document.getElementById('hoeville')
-  let currentIndex = 0;
+document.addEventListener('DOMContentLoaded', function() {
+    const wrapper = document.getElementById('hoeville');
+    let currentIndex = 0;
+    const wrapperClasses = [
+            'lemoan',
+            'pg',
+            'amateur',
+            'xtrahot',
+            'hardcore'
+    ];
+    //   //
+    //   // const handleTextBlock = (index) => {
+    //   //   const textBlock = wrapper.querySelector(`[data-hoeville="${index}"]`)
+    //   //   if(textBlock){
+    //   //     const textSections = document.querySelectorAll('.hoeville-text-wrapper')
+    //   //     for(const section of textSections){
+    //   //       if(parseInt(section.getAttribute('data-hoeville')) !== index) {
+    //   //         section.style.display = 'none'
+    //   //       }else{
+    //   //         if (section.style.removeProperty) {
+    //   //           section.style.removeProperty('display');
+    //   //         } else {
+    //   //           section.style.removeAttribute('display');
+    //   //         }
+    //   //       }
+    //   //     }
+    //   //   }
+    //   // }
+    //   //
+    const slideIndy = document.getElementById('indy');
+    var prevClass = 'lemoan'
+    const animate = (index, triggerIndex) => {
+        if (index > triggerIndex) return;
 
-  const handleTextBlock = (index) => {
-    const textBlock = wrapper.querySelector(`[data-hoeville="${index}"]`)
-    if(textBlock){
-      const textSections = document.querySelectorAll('.hoeville-text-wrapper')
-      for(const section of textSections){
-        if(parseInt(section.getAttribute('data-hoeville')) !== index) {
-          section.style.display = 'none'
-        }else{
-          if (section.style.removeProperty) {
-            section.style.removeProperty('display');
-          } else {
-            section.style.removeAttribute('display');
-          }
+
+
+        if (index === 4) {
+            document.getElementById('squirt').style.opacity = '1';
+            return;
         }
-      }
+        const section = document.getElementById(`section-${index}`);
+        section.style.display = 'block';
+        const grad1 = document.getElementById(`${index}-grad-1`);
+        grad1.beginElement();
+        const grad2 = document.getElementById(`${index}-grad-2`);
+        grad2.beginElement();
+
+        const slidePos = [
+            'calc(0% - 15px)',
+            'calc(25% - 20px)',
+            'calc(50% - 25px)',
+            'calc(75% - 30px)',
+            'calc(100% - 35px)',
+        ];
+        slideIndy.style.bottom = `${slidePos[triggerIndex -1]}`;
+    };
+
+    if (wrapper) {
+        var triggers = document.getElementsByClassName('trigger');
+
+        animate(currentIndex, 0);
+
+        for (const trigger of triggers) {
+            trigger.addEventListener('click', () => {
+                const triggerIndex = parseInt(
+                        trigger.getAttribute('data-trigger-index'));
+
+
+                if (currentIndex === triggerIndex) return;
+
+                wrapper.classList.remove(prevClass)
+                wrapper.classList.add(wrapperClasses[triggerIndex])
+                prevClass = wrapperClasses[triggerIndex];
+
+                currentIndex = triggerIndex;
+                document.getElementById('squirt').style.opacity = '0';
+                const sections = document.querySelectorAll('.sections');
+                for (const section of sections) {
+                    section.style.display = 'none';
+                }
+                for (let index = 0; index < (triggerIndex + 1); index++) {
+                    setTimeout(() => {
+                        animate(index, (triggerIndex + 1));
+                    }, (200 * index));
+                }
+
+            });
+        }
     }
-  }
-
-  const animate = (index, triggerIndex) => {
-    if (index > triggerIndex) return;
-
-    if(index === 4){
-      document.getElementById('squirt').style.opacity = '1'
-      return
-    }
-    const section = document.getElementById(`section-${index}`)
-    section.style.display = 'block'
-    const grad1 = document.getElementById(`${index}-grad-1`)
-    grad1.beginElement()
-    const grad2 = document.getElementById(`${index}-grad-2`)
-    grad2.beginElement()
-  }
-
-  if(wrapper){
-    var triggers = document.getElementsByClassName("trigger");
-    animate(currentIndex, 0)
-    handleTextBlock(0)
-    const outline = document.getElementById(`outline_0`)
-    outline.style.opacity = '1'
-
-    for (const trigger of triggers) {
-      trigger.addEventListener('mouseover', () => {
-        const triggerIndex = parseInt(trigger.getAttribute('data-trigger-index'))
-        if(currentIndex === triggerIndex) return;
-        currentIndex = triggerIndex
-        document.getElementById('squirt').style.opacity = '0'
-        const sections = document.querySelectorAll('.sections')
-        for(const section of sections){
-          section.style.display = 'none'
-        }
-        const outlines = document.querySelectorAll(`.hover-indicator`)
-        for(const outline of outlines){
-          outline.style.opacity = '0'
-        }
-        const outline = document.getElementById(`outline_${triggerIndex}`)
-        outline.style.opacity = '1'
-        handleTextBlock(triggerIndex)
-        for (let index = 0; index < (triggerIndex +1); index++) {
-          setTimeout(() => {
-            animate(index, (triggerIndex +1))
-          }, (200 * index))
-        }
-      })
-    }
-  }
 });
