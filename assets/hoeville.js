31d3821defcc6397b4c611fa09ef5fdcfac9dd68
Aiden G <emailaiden@gmail.com>
2024-11-11 13:43:22 +0200

    Fix bugs & styling, change level on label clicks

--- assets/hoeville.js
+++ assets/hoeville.js
@@ -1,92 +1,64 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const wrapper = document.getElementById('hoeville');
-    let currentIndex = 0;
-    const wrapperClasses = [
-            'lemoan',
-            'pg',
-            'amateur',
-            'xtrahot',
-            'hardcore'
-    ];
+let wrapper;
+let currentIndex = 0;
+let prevClass = "lemoan";
+const wrapperClasses = ["lemoan", "pg", "amateur", "xtrahot", "hardcore"];
 
-    // const slideIndy = document.getElementById('indy');
-    var prevClass = 'lemoan'
-    const animate = (index, triggerIndex) => {
-        if (index > triggerIndex) return;
+function animateScale(index, triggerIndex) {
+    // added second condition to prevent bug when scaling down and setTimeout callbacks for higher levels get called
+    if (index > triggerIndex || (!((currentIndex + 1) == triggerIndex))) return;
 
-
-
-        if (index === 4) {
-            document.getElementById('squirt').style.opacity = '1';
-            return;
-        }
-        const section = document.getElementById(`section-${index}`);
-        section.style.display = 'block';
-        const grad1 = document.getElementById(`${index}-grad-1`);
-        grad1.beginElement();
-        const grad2 = document.getElementById(`${index}-grad-2`);
-        grad2.beginElement();
-
-        // const slidePos = [
-        //     'calc(0% - 15px)',
-        //     'calc(25% - 20px)',
-        //     'calc(50% - 25px)',
-        //     'calc(75% - 30px)',
-        //     'calc(100% - 35px)',
-        // ];
-        // slideIndy.style.bottom = `${slidePos[triggerIndex -1]}`;
-    };
-
-    if (wrapper) {
-        const slider = document.getElementById('range-slider');
-
-        slider.addEventListener('input', function(e) {
-            const triggerIndex = e.target.value / 25
-            if (currentIndex === triggerIndex) return;
-
-            wrapper.classList.remove(prevClass)
-            wrapper.classList.add(wrapperClasses[triggerIndex])
-            prevClass = wrapperClasses[triggerIndex];
-            currentIndex = triggerIndex;
-            document.getElementById('squirt').style.opacity = '0';
-            const sections = document.querySelectorAll('.sections');
-            for (const section of sections) {
-                section.style.display = 'none';
-            }
-            for (let index = 0; index < (triggerIndex + 1); index++) {
-                setTimeout(() => {
-                    animate(index, (triggerIndex + 1));
-                }, (200 * index));
-            }
-        }, false);
-        // var triggers = document.getElementsByClassName('trigger');
-
-        animate(currentIndex, 0);
-
-        // for (const trigger of triggers) {
-        //     trigger.addEventListener('click', () => {
-        //         const triggerIndex = parseInt(
-        //                 trigger.getAttribute('data-trigger-index'));
-        //
-        //
-        //         if (currentIndex === triggerIndex) return;
-        //
-        //         wrapper.classList.remove(prevClass)
-        //         wrapper.classList.add(wrapperClasses[triggerIndex])
-        //         prevClass = wrapperClasses[triggerIndex];
-        //         currentIndex = triggerIndex;
-        //         document.getElementById('squirt').style.opacity = '0';
-        //         const sections = document.querySelectorAll('.sections');
-        //         for (const section of sections) {
-        //             section.style.display = 'none';
-        //         }
-        //         for (let index = 0; index < (triggerIndex + 1); index++) {
-        //             setTimeout(() => {
-        //                 animate(index, (triggerIndex + 1));
-        //             }, (200 * index));
-        //         }
-        //
-        //     });
-        // }
+    if (index === 4) {
+        document.getElementById("squirt").style.opacity = "1";
+        return;
     }
+    const section = document.getElementById(`section-${index}`);
+    section.style.display = "block";
+    const grad1 = document.getElementById(`${index}-grad-1`);
+    grad1.beginElement();
+    const grad2 = document.getElementById(`${index}-grad-2`);
+    grad2.beginElement();
+}
+
+function changeScale(val) {
+    const triggerIndex = val / 25;
+    if (currentIndex === triggerIndex) {
+        return;
+    }
+    currentIndex = triggerIndex;
+    wrapper.classList.remove(prevClass);
+    wrapper.classList.add(wrapperClasses[triggerIndex]);
+    prevClass = wrapperClasses[triggerIndex];
+    document.getElementById("squirt").style.opacity = "0";
+    const sections = document.querySelectorAll(".sections");
+    for (const section of sections) {
+        section.style.display = "none";
+    }
+    for (let index = 0; index < triggerIndex + 1; index++) {
+        setTimeout(() => {
+            animateScale(index, triggerIndex + 1);
+        }, 200 * index);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  wrapper = document.getElementById("hoeville");
+
+  if (wrapper) {
+    const slider = $('#range-slider');
+
+    // slider.on("input", function (e) {
+    //   slider.trigger("change");
+    // });
+    slider.on("change", function (e) {
+        changeScale($(this).val());
+    });
+
+    $('#hoeville .scale > .trigger').on('click', function() {
+        const triggerIndex = $(this).data('trigger-index');
+        slider.val(triggerIndex * 25).change();
+    });
+
+    animateScale(currentIndex, 1);
+
+  }
 });
